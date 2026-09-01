e2e88b3f4b4cc22042efc6a469d6f869c2d00da1
Newton Wamiti <144038101+Wamitinewton@users.noreply.github.com>
2026-09-01 21:38:01 +0300

    Add: longest-substring-without-duplicates - submission-3

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-3.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        // Naive Solution
+
+        int n = s.length();
+        int res = 0;
+
+        if (n == 0 || n == 1) {
+            return s.length();
+        }
+
+        for (int i = 0; i < n; i++) {
+            boolean[] vis = new boolean[128];
+
+            for (int j = i; j < n; j++) {
+                // If Current Character has been seen
+                // Break the loop
+
+                if (vis[s.charAt(j)] == true) {
+                    break;
+                } else {
+                    res = Math.max(res, j - i + 1);
+                    vis[s.charAt(j)] = true;
+                }
+            }
+        }
+
+        return res;
+    }
+}
